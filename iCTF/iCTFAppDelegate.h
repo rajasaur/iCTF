b1c71106cb13cefab3e7527e64f31f35e32adc35
Raja <raja@Rajas-Mac.local>
2011-10-20 21:09:12 +0530

    Initial Commit

---
+++ iCTF/iCTFAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  iCTFAppDelegate.h
+//  iCTF
+//
+//  Created by Raja on 20/10/11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iCTFAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ iCTF/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iCTF
+//
+//  Created by Raja on 20/10/11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
